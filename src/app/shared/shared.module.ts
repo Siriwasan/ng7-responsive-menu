913bf61891b2306d8338012e30234a248ea2adab
Siriwasan <siriwasan@gmail.com>
2019-04-16 11:14:20 +0700

    add Side Navitaion

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AngularMaterialModule } from '../shared/angular-material.module';
+import { SideNavComponent } from './components/side-nav/side-nav.component';
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule, AngularMaterialModule]
+  declarations: [SideNavComponent],
+  imports: [CommonModule, AngularMaterialModule],
+  exports: [SideNavComponent]
 })
 export class SharedModule {}
