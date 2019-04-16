1e28ac1a76f8398d6b114f4d6306b55d2aaeee8c
Siriwasan <siriwasan@gmail.com>
2019-04-16 10:45:44 +0700

    add Shared module

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AngularMaterialModule } from '../shared/angular-material.module';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, AngularMaterialModule]
+})
+export class SharedModule {}
